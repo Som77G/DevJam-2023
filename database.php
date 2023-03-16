a357c30a4ff92a3ec0577da3d9f598b1c68f31ad
anujtyagi234 <123989873+anujtyagi234@users.noreply.github.com>
2023-03-16 08:44:25 +0530

    Add files via upload

---
+++ database.php
@@ -0,0 +1,55 @@
+<?php
+// start session
+session_start();
+
+// connect to database
+$servername = "localhost";
+$username = "root";
+$password = "";
+$dbname = "register";
+
+$conn = mysqli_connect($servername, $username, $password, $dbname);
+
+// check for errors
+if (!$conn) {
+    die("Connection failed: " . mysqli_connect_error());
+}
+
+// process form data
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+    $reg_no =  $_POST["registration_no"];
+    $phone_no = $_POST["phone_no"];
+    $email_id = $_POST["email_id"];
+    $gender = $_POST["gender"];
+    $password =  $_POST["password"];
+
+    // check if registration number already exists
+    $sql = "SELECT * FROM login_data WHERE registration_no='$reg_no'";
+    $result = mysqli_query($conn, $sql);
+
+    if (!$result) {
+        die("Error executing query: " . mysqli_error($conn));
+    }
+
+    if (mysqli_num_rows($result) == 0) {
+        // registration number is available, insert new user data
+        $sql = "INSERT INTO login_data (registration_no, phone_no, email_id, gender, password)
+                VALUES ('$reg_no', '$phone_no', '$email_id', '$gender', '$password')";
+
+        if (mysqli_query($conn, $sql)) {
+            // registration succeeded, set session variables and redirect to main page
+            $_SESSION["registration_no"] = $reg_no;
+            $_SESSION["loggedin"] = true;
+            echo "<script> alert('registered successfully.') </script>";
+            echo"<script> window.open('login.html','_self')</script>";
+        } else {
+            echo "Error registering user: " . mysqli_error($conn);
+        }
+    } else {
+        echo "Registration number already exists. Please use a different registration number.";
+    }
+}
+
+// close database connection
+mysqli_close($conn);
+?>
---
+++ login.php
@@ -0,0 +1,29 @@
+<?php
+$servername = "localhost";
+$username = "root";
+$password = "";
+$dbname = "register";
+
+$conn = mysqli_connect($servername, $username, $password, $dbname);
+
+// Get login details from form
+$reg_no = $_POST['registration_no'];
+$password = $_POST['password'];
+
+// Query to check if user exists
+$sql = "SELECT * FROM login_data WHERE registration_no='$reg_no' AND password='$password'";
+$result = mysqli_query($conn, $sql);
+
+// Check if user exists and redirect to home page if successful
+if (mysqli_num_rows($result) == 1) {
+	session_start();
+	$_SESSION['registration_no'] = $reg_no;
+	header('Location: index.html');
+} else {
+	// Display error message if login is unsuccessful
+	echo 'Invalid login details. Please try again.';
+}
+
+// Close MySQL connection
+mysqli_close($conn);
+?>
