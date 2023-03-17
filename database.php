119ca29eaccd41a67f0cceea415c878e2d30af54
anujtyagi234 <123989873+anujtyagi234@users.noreply.github.com>
2023-03-17 09:33:33 +0530

    Update database.php

--- database.php
+++ database.php
@@ -19,6 +19,8 @@ if (!$conn) {
 if ($_SERVER["REQUEST_METHOD"] == "POST") {
     $name =  $_POST["name"];
     $reg_no =  $_POST["registration_no"];
+    $course =  $_POST["course"];
+    $branch =  $_POST["branch"];
     $phone_no = $_POST["phone_no"];
     $email_id = $_POST["email_id"];
     $gender = $_POST["gender"];
@@ -34,8 +36,8 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
 
     if (mysqli_num_rows($result) == 0) {
         // registration number is available, insert new user data
-        $sql = "INSERT INTO login_data (name,registration_no, phone_no, email_id, gender, password)
-                VALUES ('$name','$reg_no', '$phone_no', '$email_id', '$gender', '$password')";
+        $sql = "INSERT INTO login_data (name,registration_no,course,branch, phone_no, email_id, gender, password)
+                VALUES ('$name','$reg_no','$course','$branch', '$phone_no', '$email_id', '$gender', '$password')";
 
         if (mysqli_query($conn, $sql)) {
             // registration succeeded, set session variables and redirect to main page
