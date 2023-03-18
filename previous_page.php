e0929c69ed97599abcc52cf7444a37ed77d56a61
anujtyagi234 <123989873+anujtyagi234@users.noreply.github.com>
2023-03-18 22:58:06 +0530

    Create previous_page.php

---
+++ previous_page.php
@@ -0,0 +1,13 @@
+<?php
+session_start(); // Make sure to start the session
+
+// Check if the user is logged in
+if (!isset($_SESSION['registration_no'])) {
+    header('Location: index.html');
+    exit();
+}
+if(isset($_SESSION['registration_no'])) {
+    header('Location: protected_page.php');
+    exit();
+}
+?>
