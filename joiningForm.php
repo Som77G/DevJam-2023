9c84987c580027016293366c6cd67dd7f2effef7
anujtyagi234 <123989873+anujtyagi234@users.noreply.github.com>
2023-03-18 23:23:27 +0530

    Update joiningForm.php

--- joiningForm.php
+++ joiningForm.php
@@ -41,6 +41,9 @@ function setSubmit($conn) {
 <!DOCTYPE html>
 <html>
 <head>
+<meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
 	<title>College Committee Joining Form</title>
 	<link rel="stylesheet" href="joiningForm.css">
 </head>
