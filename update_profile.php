838579434e7d76f7ff8a09987194e398483804f3
anujtyagi234 <123989873+anujtyagi234@users.noreply.github.com>
2023-03-18 23:42:06 +0530

    Update update_profile.php

--- update_profile.php
+++ update_profile.php
@@ -42,6 +42,9 @@ $current_user = mysqli_fetch_assoc($result);
 <!DOCTYPE html>
 <html>
 <head>
+<meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Update Profile</title>
     <style>
         .heading {
