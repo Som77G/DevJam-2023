b532fa92911300efbeab13da834f158bca9371d5
smt96700 <125037882+smt96700@users.noreply.github.com>
2023-03-18 12:23:32 +0530

    Add files via upload

---
+++ comment.php
@@ -0,0 +1,122 @@
+<?php
+  date_default_timezone_set('Asia/Kolkata');
+  $conn= mysqli_connect('localhost', 'root', '', 'commentsection');
+
+  if(!$conn){
+      die("Connect failed: ".mysqli_connect_error());
+   
+  }
+      function setComments($conn){
+         if (isset($_POST['commentSubmit'])){
+            $uid = $_POST['uid'];
+            $date = $_POST['date'];
+            $message = $_POST['message'];
+     
+            $sql = "INSERT INTO comments (uid, date, message) VALUES ('$uid', '$date', '$message')";
+            $result = $conn->query($sql);
+         
+         } 
+      }
+     
+      function getComments($conn){
+         $sql = "SELECT * FROM comments";
+         $result = $conn->query($sql);
+         while($row = $result->fetch_assoc()){
+             echo "<div class= 'comment-box'>" ;
+             //echo $row['cid']."<br>";
+             echo $row['uid']."<br>";
+             echo $row['date']."<br>";
+             echo nl2br($row['message'])."<br><br>";
+             echo "</div>";
+           }
+      } 
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>  
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+   <style>
+    body{
+    background-color: #ddd;
+}
+.heading {
+    text-align: center;
+    background-color: rgb(15, 154, 124);
+    /* border: 2px solid rgb(107, 57, 21); */
+    height: 15vh;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+}
+  
+#_1{
+    float:left;
+    height:55px;
+}
+textarea{
+    width: 500px;
+    height: 80px;
+    background-color: #fff;
+    resize: none;
+}
+button{
+    width:80px;
+    height: 30px;
+    background-color: #282828;
+    border:none;
+    color: #fff;
+    font-family: aerial;
+    font-weight: aerial;
+    font-weight: 400;
+    cursor:pointer;
+    margin-bottom:60px;
+    margin-left:200px;
+    margin-top:5px;
+}
+.comment-box{
+    max-width: 500px;
+    padding: 10px;
+    margin: 5px;
+    background-color: #fff;
+    border-radius: 5px;
+    box-shadow:0 0 5px black;
+    margin-left:400px;
+}
+.para{
+    font-size:20px;
+    margin-left:120px;
+
+}
+.name{
+    padding-bottom: 0px;
+    margin-left:400px;
+} 
+   </style>
+</head>
+<body>
+    <?php
+        echo "<form method='POST' action='".setComments($conn)."'>
+            <div class='heading'>
+                <img src='download.svg' id='_1'>
+                <h1><u><i>Interact with other Users.</i></u></h1>
+            </div>
+            <br>
+            <a href='index.html'><b>| <- Go Back</b></a>
+            <br>
+            <div class='name'>  
+                <p class='para'>Name: <input type='text' name='uid' required></p>
+                <br>
+                <input type='hidden' name='date' value='".date( 'Y-m-d H:i:s')."'>
+                <textarea name='message'> </textarea><br>
+                <button type='submit' name='commentSubmit'>Comment</button>
+            </div>
+        </form>";
+
+    getComments($conn);
+    ?>
+    
+</body>
+</html>
---
+++ joiningForm.php
@@ -0,0 +1,105 @@
+<?php
+$conn= mysqli_connect('localhost', 'root', '', 'joiningform');
+
+if(!$conn){
+    die("Connect failed: ".mysqli_connect_error());
+    
+}
+
+
+function setSubmit($conn) {
+    if (isset($_POST['submit'])) {
+        // Get form data
+        $name = $_POST['name'];
+        $email = $_POST['email'];
+        $semester = $_POST['semester'];
+        $registrationNumber = $_POST['registrationNumber'];
+        $branch = $_POST['branch'];
+        $committees = implode(",", $_POST['committee']);
+        $other_committee = $_POST['other_committee'];
+        $interests = $_POST['interests'];
+        $skills = $_POST['skills'];
+        $goals = $_POST['goals'];
+
+        // Insert form data into database
+        $sql = "INSERT INTO joiningcommittees (name, email, semester, registrationNumber, branch, committee, other_committee, interests, skills, goals) VALUES ('$name', '$email', '$semester', '$registrationNumber','$branch','$committees','$other_committee','$interests','$skills','$goals')";
+       $result = $conn->query($sql);
+
+	   if($result) {
+		echo "<script> alert('Form Submitted Successfully')</script>";
+		echo "<script>window.open('index.html')</script>";
+
+	   }
+	   else {
+		echo "<script> alert('Wrong Entries')</script>";
+	   }
+    }
+}
+
+  
+?>
+<!DOCTYPE html>
+<html>
+<head>
+	<title>College Committee Joining Form</title>
+	<link rel="stylesheet" href="joiningForm.css">
+</head>
+<body>
+	<a href="index.html"><b>| <- Go Back</b></a>
+    <br>
+	<h1>College Committee Joining Form</h1>
+	<?php
+	echo "<form  method='POST' action='".setSubmit($conn)."'>
+	<label for='name'>Name:</label>
+	<input type='text' id='name' name='name' required><br><br>
+
+	<label for='email'>Email:</label>
+	<input type='email' id='email' name='email' required><br><br>
+
+	<label for='semester'>Semester:</label>
+	<input type='text' id='semester' name='semester' required><br><br>
+
+	<label for='registrationNumber'>Registration Number:</label>
+	<input type='number' id='registrationNumber' name='registrationNumber' required><br><br>
+
+
+	<label for='branch'>Branch:</label>
+	<input type='text' id='' name='branch' required><br><br>
+
+	<label>Which committee are you interested in joining? (Please check all that apply)</label><br>
+	<input type='checkbox' id='thalaivaCommittee' name='committee[]' value='thalaivaCommittee'>
+	<label for='thalaivaCommittee'>Thalaiva Committee</label><br>
+	<input type='checkbox' id='bengaliCommittee' name='committee[]' value='bengaliCommittee'>
+	<label for='bengaliCommittee'>Bengali Committee</label><br>
+	<input type='checkbox' id='rajasthaniCommittee' name='committee[]' value='rajasthaniCommittee'>
+	<label for='rajasthaniCommittee'>Rajasthani Committee</label><br>
+	<input type='checkbox' id='artsCommittee' name='committee[]' value='artsCommittee'>
+	<label for='artsCommittee'>Arts Committee</label><br>
+	<input type='checkbox' id='westernCommittee' name='committee[]' value='westernCommittee'>
+	<label for='westernCommittee'>Western Committee</label><br>
+	<input type='checkbox' id='mhmCommittee' name='committee[]' value='mhmCommittee'>
+	<label for='mhmCommittee'>MHM Committee</label><br>
+	<input type='checkbox' id='musicCommittee' name='committee[]' value='musicCommittee'>
+	<label for='musicCommittee'>Music Committee</label><br>
+	<input type='checkbox' name='committee[]' id='bhangraCommittee' value='bhangraCommittee'>
+	<label for='bhangraCommittee'>Bhangra Committee</label><br>
+	<input type='checkbox' name='committee[]' id='coreDramatics' value='coreDramatics'>
+	<label for='coreDramatics'>Core Dramatics</label><br>
+	
+	<label for='other'>Other (please specify):</label>
+	<input type='text' id='other' name='other_committee'><br><br>
+
+	<label for='interests'>Why are you interested in joining this committee(s)?</label><br>
+	<textarea id='interests' name='interests' rows='5' cols='50' required></textarea><br><br>
+
+	<label for='skills'>What skills and experiences do you bring to this committee(s)?</label><br>
+	<textarea id='skills' name='skills' rows='5' cols='50' required></textarea><br>
+
+	<label for='goals'>What are your goals?</label><br>
+	<textarea id='goals' name='goals' rows='5' cols='50' required></textarea><br>
+
+	<button type='submit' name='submit'>Submit</button>
+	</form>";
+	?>
+</body>
+</html>
