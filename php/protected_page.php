c863e54371ca46f476709d8304a9c3110ef6845e
anujtyagi234 <123989873+anujtyagi234@users.noreply.github.com>
2023-03-19 15:24:28 +0530

    Update protected_page.php

--- php/protected_page.php
+++ php/protected_page.php
@@ -29,7 +29,7 @@ while ($row = $result->fetch_assoc()) {
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta name="viewport" content="width=device-width, initial-scale=0.4">
     <title>Home</title>
     <link rel="stylesheet" href="protected_page.css">
 </head>
