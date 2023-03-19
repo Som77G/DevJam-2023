bf26aff0ceda31a19eb9d18d5ba88ed443824f0e
anujtyagi234 <123989873+anujtyagi234@users.noreply.github.com>
2023-03-19 15:24:23 +0530

    Update profile.php

--- php/profile.php
+++ php/profile.php
@@ -37,7 +37,7 @@ while ($row = $result->fetch_assoc()) {
 <head>
 <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta name="viewport" content="width=device-width, initial-scale=0.5">
   <title>Profile Page</title>
   <style>
     .heading {
